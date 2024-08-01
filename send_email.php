9e02428c27bdcd1d0a50cc3ded345be53d01b35f
Sanchit Anand <145704254+sanchitanand1@users.noreply.github.com>
2024-08-01 18:52:48 +0530

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $mobile = htmlspecialchars($_POST['mobile']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "anandsanchit198@gmail.com";
+    $headers = "From: " . $email . "\r\n" .
+               "Reply-To: " . $email . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+    $body = "Name: $name\nEmail: $email\nMobile: $mobile\n\nMessage:\n$message";
+
+    if(mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully!";
+    } else {
+        echo "Failed to send message.";
+    }
+}
+?>
